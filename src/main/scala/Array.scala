42e708cd891ce3e3f663bbc44ca9a9fb39fc9ca0
Sanket Shinde <sanket.shinde97.ss@gmail.com>
2025-10-04 20:19:15 +0530

    change in code

--- src/main/scala/Array.scala
+++ src/main/scala/Array.scala
@@ -5,9 +5,7 @@ object Array {
     println("Enter the size of array: ")
     var num=StdIn.readInt()
 
-    var arr=new Array [Int](num)
-
-
+    var arr=new Array [Int](n)
     for (i<-0 until num){
     println(s"enter the array element : arr${i+1}")
      arr(i)=StdIn.readInt()
